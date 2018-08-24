ca6375338dbf3899fe474701ec3c76ccccf28821
Alexander Gusev <san4es021@gmail.com>
2018-08-24 15:07:45 +0300

    new

---
+++ js/product.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+	$('.product-carousel').owlCarousel({
+		loop: false,
+	    margin: 1,
+	    nav: false,
+	    items: 1,
+	    thumbs: true,
+	    thumbsPrerendered: true,
+	    thumbImage: true,
+	});
+	
+	$('.info-carousel').owlCarousel({
+		loop: false,
+	    margin: 1,
+	    nav: false,
+	    items: 1,
+	    thumbs: true,
+	    URLhashListener:true,
+	    mouseDrag: false,
+	});
+
+	$(".product-carousel-wrapper  .nav-prev").click(function() {
+		$(".product-carousel .owl-nav .owl-prev").trigger('click');
+	});
+	$(".product-carousel-wrapper  .nav-next").click(function() {
+		$(".product-carousel .owl-nav .owl-next").trigger('click');
+	});
+	
+	$(".info-carousel-nav li a").click(function() {
+		$(this).parent().parent().children().removeClass('active');
+		$(this).parent().addClass('active');
+	})
+});
